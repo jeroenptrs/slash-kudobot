e99d6100970da38d64ee0aba0d28d6ecc8a092e5
Jeroen Peeters <jeroen.peeters@evance.be>
2017-09-21 22:29:08 +0200

    Refactor to user subset and admin functions

--- kudobot.js
+++ kudobot.js
@@ -2,7 +2,7 @@ module.exports = (ctx, cb) => {
     var cmds = ctx.body.text.split(" ", 2);
 
     //Filter out troll commands.
-    if(cmds.length > 0 && (cmds[0].substring(0,1) === '@' || cmds[0].toLowerCase() === 'all')){
+    if(cmds.length > 0 && (cmds[0].substring(0,1) === '@' || cmds[0].toLowerCase() === 'all' || cmds[0].toLowerCase() === 'admin')){
         ctx.storage.get(function(error, data){
             if (error) return cb(error);
 
@@ -16,10 +16,10 @@ module.exports = (ctx, cb) => {
 
                     //Set attachment data with all keys.
                     for(i = 0; i < k.length; i++){
-                        o = {text: "<" + k[i] + ">: " + data[k[i]]};
-                        if(data[k[i]] > 0)
+                        o = {text: "<" + k[i] + ">: " + data.users[k[i]]};
+                        if(data.users[k[i]] > 0)
                             o.color = "good";
-                        else if(data[k[i]] < 0)
+                        else if(data.users[k[i]] < 0)
                             o.color = "danger";
                         attachments.push(o);
                     }
@@ -30,7 +30,7 @@ module.exports = (ctx, cb) => {
                         var total = 0,
                             totalString = "";
                         for (i=0; i < k.length; i++)
-                            total += data[k[i]];
+                            total += data.users[k[i]];
                         totalString += "Together you received " + total + " kudo";
 
                         //Push to attachment data, if total != 1, add an s at the end.
@@ -62,14 +62,14 @@ module.exports = (ctx, cb) => {
                 //Is this the first time we're using this?
                 if(data === undefined) {
                     data = {};
-                    data[u] = 1;
+                    data.users[u] = 1;
                 }
 
                 //Is this the first time we're adding to this user?
-                else if (data[u] == null) { data[u] = 1; }
+                else if (data.users[u] == null) { data.users[u] = 1; }
 
                 //User exists
-                else data[u]++;
+                else data.users[u]++;
 
                 //Store that shit!
                 ctx.storage.set(data, function (error) {
@@ -80,7 +80,7 @@ module.exports = (ctx, cb) => {
             }
 
             //kudo--
-            else if(cmds[0].substring(0,1) === '@' && cmds[1] === '--'){
+            else if(cmds[0].substring(0,1) === '@' && cmds[1] === '--' && data.minmin === true){
 
 
                 //Store user name for quick use.
@@ -92,14 +92,14 @@ module.exports = (ctx, cb) => {
                 //Is this the first time we're using this?
                 if(data === undefined) {
                     data = {};
-                    data[u] = -1;
+                    data.users[u] = -1;
                 }
 
                 //Is this the first time we're adding to this user?
-                else if (data[u] == null) { data[u] = -1; }
+                else if (data.users[u] == null) { data.users[u] = -1; }
 
                 //User exists
-                else data[u]--;
+                else data.users[u]--;
 
                 //Store that shit!
                 ctx.storage.set(data, function (error) {
@@ -115,14 +115,35 @@ module.exports = (ctx, cb) => {
                 //Store user name for quick use.
                 var u = cmds[0];
 
-                if (data === undefined || data[u] == null){
+                if (data === undefined || data.users[u] == null){
                     return cb(null, { response_type: "in_channel", text: `<${cmds[0]}> has no kudos.` });
                 }
 
-                if(data[u] !== 1)
-                    return cb(null, { response_type: "in_channel", text: `<${cmds[0]}> has ${data[u]} kudos!` });
+                if(data.users[u] !== 1)
+                    return cb(null, { response_type: "in_channel", text: `<${cmds[0]}> has ${data.users[u]} kudos!` });
                 else
-                    return cb(null, { response_type: "in_channel", text: `<${cmds[0]}> has ${data[u]} kudo.` });
+                    return cb(null, { response_type: "in_channel", text: `<${cmds[0]}> has ${data.users[u]} kudo.` });
+            }
+
+            //Set an admin, for now it's just one.
+            else if(cmds[0].toLowerCase() === 'admin' && cmds[1].toLowerCase() === 'init'){
+                if(data === undefined || data.admin == null) {
+                    data.admin[data.ctx.user_name] = true;
+                    return cb(null, { text: `<@${ctx.user_name}> is now admin.` });
+                }
+                else return cb(null, { text: `An admin has already been set` });
+            }
+
+            //Enable/disable negative kudos (so /kudo @user --). Disabled on standard and needs an admin to activate.
+            else if(cmds[0].toLowerCase() === 'admin' && cmds[1] === '--' && (data.admin != null && data.admin[user] === true)){
+                if(data.minmin == null || data.minmin !== true){
+                    data.minmin = true;
+                    return cb(null, { text: `Negative kudos have been activated.` });
+                }
+                else {
+                    data.minmin = false;
+                    return cb(null, { text: `Negative kudos have been deactivated.` });
+                }
             }
 
         });
